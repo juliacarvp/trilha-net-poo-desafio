244fd61dd37258d932acf35d4c9170368a83c3ff
Júlia Carvalho <97815186+juliacarvp@users.noreply.github.com>
2024-01-06 22:38:39 -0300

    Projeto

---
+++ CriandoUmSistemaEAbstraindoUmCelular/CriandoUmSistemaEAbstraindoUmCelular/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoUmSistemaEAbstraindoUmCelular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: "64");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Smartphone iPhone:");
+            Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222", memoria: "128");
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
---
+++ CriandoUmSistemaEAbstraindoUmCelular/CriandoUmSistemaEAbstraindoUmCelular/Smartphone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoUmSistemaEAbstraindoUmCelular
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private string Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, string memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
